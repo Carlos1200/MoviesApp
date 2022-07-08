b4c7e6d16e303c14cfb384d9ca07f462bd4fad17
Carlos1200 <carlos1200herrera@gmail.com>
2022-07-07 23:02:23 -0600

    Adding SplashScreen

--- App.tsx
+++ App.tsx
@@ -1,9 +1,13 @@
 import 'react-native-gesture-handler';
-import * as React from 'react';
+import React, {useEffect} from 'react';
+import SplashScreen from 'react-native-lottie-splash-screen';
 import {NavigationContainer} from '@react-navigation/native';
 import {Navigation} from './src/navigation/Navigation';
 
 export default function App() {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
   return (
     <NavigationContainer>
       <Navigation />
